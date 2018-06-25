491e7a6debf4911f541f1bb366db3a4eb06c2dea
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-06-25 20:36:14 +0300

    add  hibernate lessons up to 9

---
+++ hibernate/src/main/java/JPAManyToMany/Role.java
@@ -0,0 +1,53 @@
+package JPAManyToMany;
+
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<User>();
+
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public Role(long id) {this.id = id;}
+    public Role() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
