15768e94473a8e92f6a287409dd506ae0d76ec3d
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-06-25 20:16:51 +0300

    add  hibernate lessons up to 8

---
+++ hibernate/src/main/java/JPAOneToOne/Role.java
@@ -0,0 +1,53 @@
+package JPAOneToOne;
+
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @OneToOne(mappedBy = "role")
+    private User user;
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role(long id) {this.id = id;}
+    public Role() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
