b5a807246257a76348a9605aef61d53b0d5e6f08
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-06-26 07:31:23 +0300

    add  hibernate lessons up to 11

--- hibernate/src/main/java/Main.java
+++ hibernate/src/main/java/Main.java
@@ -1,7 +1,29 @@
+
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+
+
 public class Main {
+
+   // private Logger logger = Logger.getInstance(Main.class);
+
         public static void main(String[] args) {
             SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session session = sessionFactory.openSession();
+
+            try {
+                session.beginTransaction();
+                session.getTransaction().commit();
+            }catch (Exception e){
+                session.getTransaction().rollback();
+                e.printStackTrace();
+            }finally {
+                session.close();
+                sessionFactory.close();
+            }
+
+            sessionFactory.close();
         }
 }
