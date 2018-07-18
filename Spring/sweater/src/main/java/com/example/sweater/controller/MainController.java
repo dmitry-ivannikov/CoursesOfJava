172504d8e229c1f70c9b26b6bf64d3b28cbcd7aa
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-07-18 08:54:02 +0300

    add registration up to 3

--- Spring/sweater/src/main/java/com/example/sweater/controller/MainController.java
+++ Spring/sweater/src/main/java/com/example/sweater/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.sweater;
+package com.example.sweater.controller;
 
 import com.example.sweater.domain.Message;
 import com.example.sweater.repos.MessageRepo;
@@ -14,19 +14,17 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object > model){
         Iterable<Message> messages =  messageRepo.findAll();
 
@@ -34,7 +32,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object > model){
         Message message = new Message(text, tag);
         messageRepo.save(message);
@@ -55,9 +53,6 @@ public class GreetingController {
         } else {
             messages = messageRepo.findAll();
         }
-
-
-
 
         model.put("messages", messages);
 
