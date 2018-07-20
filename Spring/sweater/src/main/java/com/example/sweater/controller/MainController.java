92c99c92e1c2ea1a8482a4479fcd03cadd1718de
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-07-20 08:50:23 +0300

    add freemarker up to 5

--- Spring/sweater/src/main/java/com/example/sweater/controller/MainController.java
+++ Spring/sweater/src/main/java/com/example/sweater/controller/MainController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -23,14 +22,21 @@ public class MainController {
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
 
-        return "greeting";
+        return "greeting.ftl";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object > model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages =  messageRepo.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()){
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -51,18 +57,5 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter, Map<String, Object > model){
-        Iterable<Message> messages;
 
-        if (filter != null && !filter.isEmpty()){
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
 }
