c7f30e94b229e56195d8359d5f4e0a2a2187d4d3
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-08-08 09:11:44 +0300

    add recaptcha  up to 12

--- Spring/sweater/src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ Spring/sweater/src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -40,6 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
+                .and().rememberMe()
                 .and()
                 .logout()
                 .permitAll();
--- Spring/sweater/src/main/java/com/example/sweater/domain/User.java
+++ Spring/sweater/src/main/java/com/example/sweater/domain/User.java
@@ -13,17 +13,12 @@ import java.util.Set;
 @Table(name = "usr")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @NotBlank(message = "Username cannot be empty")
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
-
-    @Transient
-    @NotBlank(message = "Password confirmation cannot be empty")
-    private String password2;
     private boolean active;
 
     @Email(message = "Email is not correct")
@@ -36,7 +31,7 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    public boolean isAdmin(){
+    public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
     }
 
@@ -119,13 +114,5 @@ public class User implements UserDetails {
 
     public void setActivationCode(String activationCode) {
         this.activationCode = activationCode;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 }
