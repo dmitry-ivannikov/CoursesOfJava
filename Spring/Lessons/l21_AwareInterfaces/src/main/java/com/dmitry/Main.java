f860ef461195e210ab96c8ac832fcea34ca72275
Ivannikov Dmitry <ivannikov.dmitry.v@gmail.com>
2018-11-14 06:16:15 +0300

    add spring lessons up to 21

--- Spring/Lessons/l21_AwareInterfaces/src/main/java/com/dmitry/Main.java
+++ Spring/Lessons/l21_AwareInterfaces/src/main/java/com/dmitry/Main.java
@@ -3,15 +3,16 @@ package com.dmitry;
 import com.dmitry.impl.Toyota;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
       //for config
-        //ApplicationContext context = new AnnotationConfigApplicationContext(com.Config.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(com.dmitry.Config.class);
 
         //for xml
-      ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:config.xml");
+      //ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:config.xml");
 
 
         Toyota bean = context.getBean(Toyota.class);
--- Spring/Lessons/l21_AwareInterfaces/src/main/java/com/dmitry/impl/Toyota.java
+++ Spring/Lessons/l21_AwareInterfaces/src/main/java/com/dmitry/impl/Toyota.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -16,49 +18,22 @@ import javax.annotation.PreDestroy;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
-public abstract class Toyota implements Car, InitializingBean, DisposableBean, BeanPostProcessor {
-
-    public void init(){
-        System.out.println("init");
-    }
+public abstract class Toyota implements Car, ApplicationContextAware {
 
 
-    public void afterPropertiesSet() {
-        System.out.println("afterProperties");
-    }
-
-    public void destroy() {
-        System.out.println("destroy");
-    }
-
-    @PostConstruct
-    public void postConstict(){
-        System.out.println("postConstict");
-    }
-
-    @PreDestroy
-    public void preDestroy(){
-        System.out.println("preDestroy");
-    }
-
-
-
-
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("bean init");
-        return bean;
-    }
-
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("bean post init");
-        return bean;
-    }
 
     @Lookup
     public abstract Radio getRadio();
 
+    ApplicationContext context;
+
     public void drive() {
         getRadio().listenMusic();
         System.out.println("driving by" );
+        System.out.println(new Double(context.getStartupDate()));
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
     }
 }
